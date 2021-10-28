4c62f2cc1ba52f56b3b315fc6f2c7144a0cc2cfe
Nolan Blew <nolanblew@hotmail.com>
2021-10-28 12:30:37 -0700

    chore: update publish script to allow for custom versioning

--- publish.bat
+++ publish.bat
@@ -1,3 +1,15 @@
-dotnet publish -r win-x64 --configuration Release --self-contained true -p:PublishSingleFile=true -p:Version="999.0.0" -p:IncludeAllContentForSelfExtract=true -p:IncludeNativeLibrariesForSelfExtract=true
-dotnet publish -r linux-x64 --configuration Release --self-contained true -p:PublishSingleFile=true -p:Version="999.0.0" -p:IncludeAllContentForSelfExtract=true -p:IncludeNativeLibrariesForSelfExtract=true
-dotnet publish -r osx-x64 --configuration Release --self-contained true -p:PublishSingleFile=true -p:Version="999.0.0" -p:IncludeAllContentForSelfExtract=true -p:IncludeNativeLibrariesForSelfExtract=true
+@ECHO OFF
+REM A publish script that takes in an optional argument to specify the version number.
+REM If no version number is specified, the version will be 999.0.0
+REM If a version number is specified, it will be used.
+
+SET VERSION=%1
+IF "%VERSION%" == "" (
+  SET VERSION="999.0.0"
+)
+
+ECHO "Publishing version %VERSION%"
+
+dotnet publish -r win-x64 --configuration Release --self-contained true -p:PublishSingleFile=true -p:Version="%VERSION%" -p:IncludeAllContentForSelfExtract=true -p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish -r linux-x64 --configuration Release --self-contained true -p:PublishSingleFile=true -p:Version="%VERSION%" -p:IncludeAllContentForSelfExtract=true -p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish -r osx-x64 --configuration Release --self-contained true -p:PublishSingleFile=true -p:Version="%VERSION%" -p:IncludeAllContentForSelfExtract=true -p:IncludeNativeLibrariesForSelfExtract=true
